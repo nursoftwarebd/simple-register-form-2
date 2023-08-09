406cf74870cba87cfaeb75311c727a5d459e22b2
alamincoders <developeralamin03@gmail.com>
2023-08-09 13:01:12 +0600

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,72 +1,143 @@
-// eslint-disable-next-line no-unused-vars
-import {
-  AppBar,
-  Button,
-  Container,
-  FormControl,
-  InputLabel,
-  Link,
-  MenuItem,
-  Select,
-  Stack,
-} from "@mui/material";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import { useState } from "react";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 import logo from "../assets/logo.png";
+import { ButtonBase } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-const Header = () => {
-  const [age, setAge] = useState("");
+const pages = ["Login"];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const [language, setLanguage] = React.useState("");
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        position: "static",
-        padding: "30px 0px",
-      }}
-    >
-      <AppBar
-        color="transparent"
-        sx={{
-          width: "100%",
-          boxShadow: "none",
-          borderBottom: "1px solid #E8E8E8",
-          padding: "2px 0px",
-        }}
-      >
-        <Container
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Link href="#">
-            <img width={120} height={35} src={logo} alt="logo" />
-          </Link>
+    <AppBar position="static" sx={{ backgroundColor: "#fff", boxShadow: "none", borderBottom: "1px solid #eee", padding: "18px 0" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <img src={logo} alt="logo" loading="lazy" />
+          </Box>
 
-          <Stack direction="row" spacing={2}>
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel id="demo-select-small-label">Language</InputLabel>
-              <Select
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                value={age}
-                label="Language"
-                onChange={(event) => setAge(event.target.value)}
+          <Box
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+
+              color: "inherit",
+            }}
+          >
+            <img src={logo} alt="logo" loading="lazy" />
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="#000"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
+            <Tooltip title="Open settings">
+              <FormControl sx={{ mx: 1, minWidth: 80 }} size="small">
+                <InputLabel id="demo-select-small-label">Lang</InputLabel>
+                <Select labelId="demo-select-small-label" id="demo-select-small" value={language} label="Lang" onChange={handleChange}>
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="NL">NL</MenuItem>
+                  <MenuItem value="English">English</MenuItem>
+                </Select>
+              </FormControl>
+              <ButtonBase
+                type="submit"
+                variant="contained"
+                sx={{
+                  color: "#00c0b3",
+                  background: "#fff",
+                  marginLeft: "2rem",
+                  padding: "12px 22px",
+                  border: "2px solid #00c0b3",
+                  borderRadius: "30px",
+                  fontWeight: "bold",
+                  cursor: "pointer",
+                }}
               >
-                <MenuItem value={"en"}>EN</MenuItem>
-                <MenuItem value={"nl"}>NL</MenuItem>
-                <MenuItem value={"bn"}>BN</MenuItem>
-              </Select>
-            </FormControl>
-
-            <Button variant="outlined">Login</Button>
-          </Stack>
-        </Container>
-      </AppBar>
-    </Box>
+                Login
+              </ButtonBase>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-};
-
+}
 export default Header;
