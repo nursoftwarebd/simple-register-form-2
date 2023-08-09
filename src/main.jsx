c366e5b66145a5c0d761b396932c19d496814f45
alamincoders <developeralamin03@gmail.com>
2023-08-09 18:41:09 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import Layout from "./components/Layout.jsx";
 import "./index.css";
 import App from "./pages/App.jsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Nunito, sans-serif",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout>
+        <App />
+      </Layout>
+    </ThemeProvider>
   </React.StrictMode>
 );
